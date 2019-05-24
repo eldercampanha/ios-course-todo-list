da2ddfe31cd126de11de6dfa62db4b95a80e83e6
Elder <elder@label-engine.com>
2019-05-23 18:31:51 -0700

    Extending class with UITableViewDataSource, and UITableViewDelegate.
    Adding required methods:
    * number of rows,
    * cell for row,
    * will Display Cell.
    
    Setting ViewController to be datasource and delegate of tableView,
    
    Adding fake and static data for testing proposes

--- todo list/ViewController.swift
+++ todo list/ViewController.swift
@@ -8,14 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+
+    // MARK: - Outlets
 
     @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Setting this class as responsible for providing data,
+        // and manage events from the tableView
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
     }
 
+    // MARK: - UITableViewDelegate, UITableViewDataSource
+    
+    // How many cells (items) or table view should display
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    // Provide Cell (or component) to be displayed on each row of our tableView
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Loading Cell from tableview using the identifier
+        // that we set earlier on Main.storyboard : identifier = "Cell"
+        return tableView.dequeueReusableCell(withIdentifier: "Cell")!
+    }
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        // Adding a text to our cell so we can test it
+        cell.textLabel?.text = "Cee"
+    }
 
 }
 
